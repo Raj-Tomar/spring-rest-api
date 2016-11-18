995ee8bce229c1fc65fab8b187509a364ccb547e
Raj Tomar <rajtomar50@gmail.com>
2016-11-18 16:28:35 +0530

    swagger-springmvc removed from application
    Signed-off-by: Raj Tomar <rajtomar50@gmail.com>

--- spring-rest-api/src/main/java/com/raj/controller/GreetingController.java
+++ spring-rest-api/src/main/java/com/raj/controller/GreetingController.java
@@ -26,7 +26,7 @@ public class GreetingController {
 	@ApiOperation(value = "getGreeting", nickname = "getGreeting")
 	@RequestMapping(method = RequestMethod.GET, path = "/greeting", produces = "application/json")
 	@ApiImplicitParams({
-			@ApiImplicitParam(name = "name", value = "User's name", required = false, dataType = "string", paramType = "query", defaultValue = "Niklas") })
+			@ApiImplicitParam(name = "name", value = "User's name", required = false, dataType = "string", paramType = "query", defaultValue = "Raj") })
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success", response = Greeting.class),
 			@ApiResponse(code = 401, message = "Unauthorized"), @ApiResponse(code = 403, message = "Forbidden"),
 			@ApiResponse(code = 404, message = "Not Found"), @ApiResponse(code = 500, message = "Failure") })
