c39bac8e3feaebc7f06b5ce031fe3dfba3fc2491
Raj Tomar <rajtomar50@gmail.com>
2017-02-02 10:32:43 +0530

    PropertyPlaceholderConfigurer implemented for reading properties file from environment variable.
    Signed-off-by: Raj Tomar <rajtomar50@gmail.com>

--- spring-rest-api/src/main/java/com/raj/app/configuration/HibernateConfiguration.java
+++ spring-rest-api/src/main/java/com/raj/app/configuration/HibernateConfiguration.java
@@ -6,6 +6,7 @@ import javax.sql.DataSource;
 
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -21,7 +22,103 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class HibernateConfiguration {
 
 	@Autowired
-    private Environment environment;
+	private Environment environment;
+
+	// Hibernate Properties Configuration Start
+
+	@Bean(name="propertyConfigurer")
+	public static PropertyPlaceholderConfigurer properties(){
+		PropertyUtilConfigurer ppc = new PropertyUtilConfigurer();
+		/*Resource[] resources = new ClassPathResource[ ]
+					{ new ClassPathResource( "application.properties" ) };
+			ppc.setLocations( resources );*/
+		ppc.setFileName("application.properties");
+		ppc.setIgnoreUnresolvablePlaceholders( true );
+		return ppc;
+	}
+
+	private Properties getHibernateProperties() {
+		Properties properties = new Properties();
+		properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+		properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+		properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+		return properties;
+	}
+	// Hibernate Properties Configuration End
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	// Multiple Database Configuration Start
+
+
+	// DataSource Configuration Start
+	@Bean(name = "dataSource")
+	public DataSource getDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.connection.driver_class"));
+		dataSource.setUrl(environment.getRequiredProperty("jdbc.connection.url.test"));
+		dataSource.setUsername(environment.getRequiredProperty("jdbc.connection.username"));
+		dataSource.setPassword(environment.getRequiredProperty("jdbc.connection.password"));
+		return dataSource;
+	}
+
+	@Bean(name = "dataSourceWorld")
+	public DataSource getDataSourceWorld() {
+		DriverManagerDataSource dataSourceWorld = new DriverManagerDataSource();
+		dataSourceWorld.setDriverClassName(environment.getRequiredProperty("jdbc.connection.driver_class"));
+		dataSourceWorld.setUrl(environment.getRequiredProperty("jdbc.connection.url.world"));
+		dataSourceWorld.setUsername(environment.getRequiredProperty("jdbc.connection.username"));
+		dataSourceWorld.setPassword(environment.getRequiredProperty("jdbc.connection.password"));
+		return dataSourceWorld;
+	}
+	// DataSource Configuration End
+
+	// SessionFactory Configuration Start
+
+	@Autowired
+	@Bean(name = "sessionFactory")
+	public SessionFactory getSessionFactory(DataSource dataSource) {
+		LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
+		sessionBuilder.scanPackages("com.raj.beans");
+		sessionBuilder.addProperties(getHibernateProperties());
+		return sessionBuilder.buildSessionFactory();
+	}
+
+	@Autowired
+	@Bean(name = "sessionFactoryWorld")
+	public SessionFactory getSessionFactoryWorld(DataSource dataSourceWorld) {
+		LocalSessionFactoryBuilder sessionBuilderWorld = new LocalSessionFactoryBuilder(dataSourceWorld);
+		sessionBuilderWorld.scanPackages("com.raj.beans");
+		sessionBuilderWorld.addProperties(getHibernateProperties());
+		return sessionBuilderWorld.buildSessionFactory();
+	}
+
+	// SessionFactory Configuration End
+
+	// TransactionManager Configuration Start
+
+	@Autowired
+	@Bean(name = "transactionManager")
+	public HibernateTransactionManager getTransactionManager(
+			SessionFactory sessionFactory) {
+		HibernateTransactionManager transactionManager = new HibernateTransactionManager(
+				sessionFactory);
+		return transactionManager;
+	}
+
+	@Autowired
+	@Bean(name = "transactionManagerWorld")
+	public HibernateTransactionManager getTransactionManagerWorld(
+			SessionFactory sessionFactoryWorld) {
+		HibernateTransactionManager transactionManagerWorld = new HibernateTransactionManager(
+				sessionFactoryWorld);
+		return transactionManagerWorld;
+	}
+	// TransactionManager Configuration End
+
+
+	// Multiple Database Configuration End
+	
 	
 	/*
 	@Bean(name = "dataSource")
@@ -33,31 +130,31 @@ public class HibernateConfiguration {
 	    dataSource.setPassword(environment.getRequiredProperty("jdbc.connection.password"));
 	    return dataSource;
 	}
-	
+
 	@Autowired
 	@Bean(name = "sessionFactory")
 	public SessionFactory getSessionFactory(DataSource dataSource) {
-	 
+
 	    LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
-	 
-	    
+
+
 	    sessionBuilder.addAnnotatedClasses(CityBean.class);
 	    sessionBuilder.addAnnotatedClasses(CountryBean.class);
 	    sessionBuilder.addAnnotatedClasses(DepartmentBean.class);
 	    sessionBuilder.addAnnotatedClasses(DeptContactDetail.class);
 	    sessionBuilder.addAnnotatedClasses(EmployeeBean.class);
-	    
-	   
+
+
 	    //sessionBuilder.addAnnotatedClasses(CityBean.class, CountryBean.class);
 	    sessionBuilder.scanPackages("com.raj.beans");
-	    
+
 	    //sessionBuilder.setProperty("hibernate.show_sql", "true");
-	    
+
 	    sessionBuilder.addProperties(getHibernateProperties());
-	 
+
 	    return sessionBuilder.buildSessionFactory();
 	}
-	
+
 	private Properties getHibernateProperties() {
 	    Properties properties = new Properties();
 	    properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
@@ -65,98 +162,16 @@ public class HibernateConfiguration {
 	    properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
 	    return properties;
 	}
-	
+
 	@Autowired
 	@Bean(name = "transactionManager")
 	public HibernateTransactionManager getTransactionManager(
 	        SessionFactory sessionFactory) {
 	    HibernateTransactionManager transactionManager = new HibernateTransactionManager(
 	            sessionFactory);
-	 
+
 	    return transactionManager;
 	}
-	
-	*/
-	
-	//////////////////////////////////////////////////////////////////////////////////////////////////////////
-	
-	// Multiple Database Configuration Start
-    
-    // DataSource Configuration Start
-    @Bean(name = "dataSource")
-	public DataSource getDataSource() {
-    	DriverManagerDataSource dataSource = new DriverManagerDataSource();
-	    dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.connection.driver_class"));
-	    dataSource.setUrl(environment.getRequiredProperty("jdbc.connection.url.test"));
-	    dataSource.setUsername(environment.getRequiredProperty("jdbc.connection.username"));
-	    dataSource.setPassword(environment.getRequiredProperty("jdbc.connection.password"));
-	    return dataSource;
-	}
-    
-    @Bean(name = "dataSourceWorld")
-	public DataSource getDataSourceWorld() {
-    	DriverManagerDataSource dataSourceWorld = new DriverManagerDataSource();
-		dataSourceWorld.setDriverClassName(environment.getRequiredProperty("jdbc.connection.driver_class"));
-		dataSourceWorld.setUrl(environment.getRequiredProperty("jdbc.connection.url.world"));
-		dataSourceWorld.setUsername(environment.getRequiredProperty("jdbc.connection.username"));
-		dataSourceWorld.setPassword(environment.getRequiredProperty("jdbc.connection.password"));
-	    return dataSourceWorld;
-	}
-    // DataSource Configuration End
-    
-    // SessionFactory Configuration Start
-    
-	@Autowired
-	@Bean(name = "sessionFactory")
-	public SessionFactory getSessionFactory(DataSource dataSource) {
-	    LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
-	    sessionBuilder.scanPackages("com.raj.beans");
-	    sessionBuilder.addProperties(getHibernateProperties());
-	    return sessionBuilder.buildSessionFactory();
-	}
-	
-	@Autowired
-	@Bean(name = "sessionFactoryWorld")
-	public SessionFactory getSessionFactoryWorld(DataSource dataSourceWorld) {
-	    LocalSessionFactoryBuilder sessionBuilderWorld = new LocalSessionFactoryBuilder(dataSourceWorld);
-	    sessionBuilderWorld.scanPackages("com.raj.beans");
-	    sessionBuilderWorld.addProperties(getHibernateProperties());
-	    return sessionBuilderWorld.buildSessionFactory();
-	}
-	
-	// SessionFactory Configuration End
-	
-	// TransactionManager Configuration Start
-	
-	@Autowired
-	@Bean(name = "transactionManager")
-	public HibernateTransactionManager getTransactionManager(
-			SessionFactory sessionFactory) {
-		HibernateTransactionManager transactionManager = new HibernateTransactionManager(
-				sessionFactory);
-		return transactionManager;
-	}
-    
-	@Autowired
-	@Bean(name = "transactionManagerWorld")
-	public HibernateTransactionManager getTransactionManagerWorld(
-			SessionFactory sessionFactoryWorld) {
-		HibernateTransactionManager transactionManagerWorld = new HibernateTransactionManager(
-				sessionFactoryWorld);
-		return transactionManagerWorld;
-	}
-	// TransactionManager Configuration End
-	
-	// Hibernate Properties Configuration Start
-	
-	private Properties getHibernateProperties() {
-	    Properties properties = new Properties();
-	    properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
-	    properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
-	    properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
-	    return properties;
-	}
-	// Hibernate Properties Configuration End
-	
-    // Multiple Database Configuration End
+
+	 */
 }
---
+++ spring-rest-api/src/main/java/com/raj/app/configuration/PropertyUtilConfigurer.java
@@ -0,0 +1,73 @@
+package com.raj.app.configuration;
+
+import java.util.Properties;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+
+public class PropertyUtilConfigurer extends PropertyPlaceholderConfigurer {
+
+	private String propertiesfileName;
+	private final String APP_HOME = "APP_HOME";
+
+	private Properties appProperties = new Properties();
+
+	private final String PROPERTIES_FILE = File.separator;// + "APP_HOME" +
+															// File.separator;
+
+	@Override
+	public void setProperties(Properties properties) {
+		super.setProperties(properties);
+	}
+
+	public String fetchRequestDumperHomePath() {
+		String ucm_home_path = System.getProperty(APP_HOME);
+		if (ucm_home_path == null) {
+			logger.info("Not able to get system property " + APP_HOME + ". Trying to get from system environment.");
+			ucm_home_path = System.getenv(APP_HOME);
+		}
+		if (ucm_home_path == null) {
+			throw new RuntimeException(
+					"UCM/Request-Dumper Home system/envrionment property is not sepecified as a JVM arg; use the: ["
+							+ APP_HOME + "] "
+							+ "to define it. E.g., -APP_HOME=/home/APP_HOME or set as environment variable.");
+		} else {
+			return ucm_home_path;
+		}
+	}
+
+	@Override
+	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+		String propertiesFilePath = null;
+		try {
+			String ucm_home = fetchRequestDumperHomePath();
+			propertiesFilePath = ucm_home + PROPERTIES_FILE + getFileName();
+			logger.info("Reading properties file: " + propertiesFilePath);
+
+			appProperties.load(new FileInputStream(propertiesFilePath));
+
+			setProperties(appProperties);
+		} catch (IOException ex) {
+			logger.error("Unable to read properties file at " + propertiesFilePath);
+		}
+		super.postProcessBeanFactory(beanFactory);
+	}
+
+	@Override
+	protected void loadProperties(Properties props) throws IOException {
+		super.loadProperties(props);
+	}
+
+	public void setFileName(String fileName) {
+		this.propertiesfileName = fileName;
+	}
+
+	public String getFileName() {
+		return propertiesfileName;
+	}
+
+}
